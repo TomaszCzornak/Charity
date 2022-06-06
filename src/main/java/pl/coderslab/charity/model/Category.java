4ff644f3b24301223f35285f7c57eb11af3186c3
TomaszCzornak <tomasz.czornak59@gmail.com>
2022-06-06 19:30:12 +0200

    ENTITIES

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,30 @@
+package pl.coderslab.charity.model;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/pl/coderslab/charity/repository/Category.java
@@ -0,0 +1,8 @@
+package pl.coderslab.charity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Category extends JpaRepository<Integer, Category> {
+
+
+}
